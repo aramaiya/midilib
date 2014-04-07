1f6238cc763a4ef8dc8622cc591c7c218df17d16
Amit Ramaiya <amitface@gmail.com>
2014-04-06 20:53:59 -0400

    get delta time

--- midi.py
+++ midi.py
@@ -7,19 +7,21 @@ class Midi:
         self.timedivision = 0
 
     def load_from_file(self, file):
-
         with open(file, 'rb') as self.midifile:
             self._readheader()
+            self._readtrack()
+            #self._readtrack()
 
     def _readheader(self):
-        header_chunk_id = self.midifile.read(4).decode('utf-8')
-        print("header chunk id: " + header_chunk_id)
-        if header_chunk_id != 'MThd':
+        header_chunkid = self.midifile.read(4).decode('utf-8')
+        print("header chunk id: %s" % header_chunkid)
+
+        if header_chunkid != 'MThd':
             print('not midi file')
             return
 
-        header_chunk_size = int.from_bytes(self.midifile.read(4), byteorder='big')
-        print("header chunk size: %d" % header_chunk_size)
+        header_chunksize = int.from_bytes(self.midifile.read(4), byteorder='big')
+        print("header chunk size: %d" % header_chunksize)
 
         self.formattype = int.from_bytes(self.midifile.read(2), byteorder='big')
         print("format type: %d" % self.formattype)
@@ -30,4 +32,33 @@ class Midi:
         self.timedivision = int.from_bytes(self.midifile.read(2), byteorder='big')
         print("time division: %d" % self.timedivision)
 
+    def _readtrack(self):
+        track_chunkid = self.midifile.read(4).decode('utf-8')
+        print("track chunk id: %s" % track_chunkid)
 
+        track_chunksize = int.from_bytes(self.midifile.read(4), byteorder='big')
+        print("track chunk size: %d" % track_chunksize)
+
+        #todo: can track chunk size = 0?
+
+        delta_time = 0
+
+        byteval = int.from_bytes(self.midifile.read(1), byteorder='big')
+
+        while (byteval & 128) != 0:
+            delta_time = (delta_time << 7) + (byteval - 128)
+            byteval = int.from_bytes(self.midifile.read(1), byteorder='big')
+
+        delta_time = (delta_time << 7) + byteval
+
+        print("delta time: %s" % delta_time)
+
+
+class Track:
+
+    def __init__(self):
+        self.deltatime = 0
+
+
+
+Midi().load_from_file('/users/amit/downloads/c-major-scale.mid')
