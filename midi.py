92451b017b9fa1ec8e0590b1b7b88f8ef610644e
Amit Ramaiya <amitface@gmail.com>
2014-04-08 01:05:37 -0400

    properly read event data

--- midi.py
+++ midi.py
@@ -1,36 +1,36 @@
 class Midi:
 
-    def __init__(self):
+    def __init__(self, file):
         self.midifile = None
         self.formattype = 0
         self.trackcount = 0
         self.timedivision = 0
+        self.metadata = {}
 
-    def load_from_file(self, file):
         with open(file, 'rb') as self.midifile:
             self._readheader()
-            self._readtrack()
-            #self._readtrack()
+            for _ in range(self.trackcount):
+                self._readtrack()
 
     def _readheader(self):
         header_chunkid = self.midifile.read(4).decode('utf-8')
-        print("header chunk id: %s" % header_chunkid)
+        self.metadata["header chunk id"] = header_chunkid
 
         if header_chunkid != 'MThd':
             print('not midi file')
             return
 
         header_chunksize = int.from_bytes(self.midifile.read(4), byteorder='big')
-        print("header chunk size: %d" % header_chunksize)
+        self.metadata["header chunk size"] = header_chunksize
 
         self.formattype = int.from_bytes(self.midifile.read(2), byteorder='big')
-        print("format type: %d" % self.formattype)
+        self.metadata["format type"] = self.formattype
 
         self.trackcount = int.from_bytes(self.midifile.read(2), byteorder='big')
-        print("number of tracks: %d" % self.trackcount)
+        self.metadata["track count"] = self.trackcount
 
         self.timedivision = int.from_bytes(self.midifile.read(2), byteorder='big')
-        print("time division: %d" % self.timedivision)
+        self.metadata["time division"] = self.timedivision
 
     def _readtrack(self):
         track_chunkid = self.midifile.read(4).decode('utf-8')
@@ -41,49 +41,75 @@ class Midi:
 
         #todo: can track chunk size = 0?
 
-
         bytecount = 1
+
         while bytecount <= track_chunksize:
             delta_time = 0
             byteval = int.from_bytes(self.midifile.read(1), byteorder='big')
-            event_bytecount = 1
+
+            bytecount += 1
+
             while (byteval & 128) != 0:
                 delta_time = (delta_time << 7) + (byteval - 128)
                 byteval = int.from_bytes(self.midifile.read(1), byteorder='big')
-                event_bytecount += 1
+
+                bytecount += 1
 
             delta_time = (delta_time << 7) + byteval
 
             print("delta time: %d" % delta_time)
 
             event_type = int.from_bytes(self.midifile.read(1), byteorder='big')
-            print("event type: %d" % event_type)
+            bytecount += 1
+            print("event type: %s" % hex(event_type))
 
             if event_type == 255:
                 meta_event_command = int.from_bytes(self.midifile.read(1), byteorder='big')
-                print("event command: %d" % meta_event_command)
+
+                #print("event command: %s" % hex(meta_event_command))
                 meta_event_length = int.from_bytes(self.midifile.read(1), byteorder='big')
+
                 event_params = self.midifile.read(meta_event_length)
-                print("event params: %s" % str(list(event_params)))
-            bytecount += 1
+                #print("event params: %s" % str(list(event_params)))
+                bytecount += meta_event_length + 1 + 1
+
+            elif 192 <= event_type <= 223:
+                data_1 = int.from_bytes(self.midifile.read(1), byteorder='big')
+                #print("data1: %d" % data_1)
+                bytecount += 1
+
+            else:
+                data_1 = int.from_bytes(self.midifile.read(1), byteorder='big')
+                data_2 = int.from_bytes(self.midifile.read(1), byteorder='big')
+                print("data1: %s" % note(data_1))
+                print("data2: %d" % data_2)
+                bytecount += 2
+
+    def __str__(self):
+        return str(self.metadata)
 
 
+def note(num):
 
+    if num < 0 or num > 127:
+        raise ValueError("Involid note code")
 
+    notes = {
+        0: 'C',
+        1: 'C#',
+        2: 'D',
+        3: 'D#',
+        4: 'E',
+        5: 'F',
+        6: 'F#',
+        7: 'G',
+        8: 'G#',
+        9: 'A',
+        10: 'A#',
+        11: 'B'
+    }
 
-class Event:
+    return notes[num % 12] + ' ' + str(int(num / 12))
 
-    def __init__(self):
-        self.deltatime = 0
+print(Midi('/users/amit/downloads/a-major-scale.mid'))
 
-
-class Track:
-
-    def __init__(self):
-        pass
-
-
-
-
-
-Midi().load_from_file('/users/amit/downloads/c-major-scale.mid')
